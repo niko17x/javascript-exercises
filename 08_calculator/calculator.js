5111f84466c6d16acce76688ebbe50881fa19431
Niko Shin <nshin17@gmail.com>
2022-05-20 17:00:51 -0700

    All exercises completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	return num.reduce((accumulator, currentValue) => accumulator += currentValue, 0);
+};
+// [2, 4] => 8;
+const multiply = function(num) {
+	return num.reduce((accumulator, currentValue) => accumulator *= currentValue, 1);
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	return (num1 ** num2);
+	return (Math.pow(num1, num2)); // solutions method.
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let num = 1;
+	if (n === 0) return 1;
+	for (let i=n; i>0; i--) {
+		num *= i; // 5 * 4 * 3 * 2 * 1
+	}
+	return num;
 };
 
 // Do not edit below this line
