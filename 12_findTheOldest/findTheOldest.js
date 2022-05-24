40a20e11226d83cc08957928d5e0c9624d63b55f
Niko Shin <nshin17@gmail.com>
2022-05-24 11:51:54 -0700

    Completed all exercises and TOP solution provided

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return currentAge > oldestAge ? currentPerson : oldest;
+    })
 };
+
+// check if 'death' is a key in the object:
+const getAge = function(birth, death) {
+    if (!death) {
+        death = (new Date().getFullYear());
+    }
+    return (death - birth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
