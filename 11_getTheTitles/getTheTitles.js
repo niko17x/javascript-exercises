4431b933d129b5c133279dce83aca681e6a08630
Niko Shin <nshin17@gmail.com>
2022-05-23 19:32:49 -0700

    Completed all exercises and TOP solution added

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(value) {
+    const getTitles = value.map(value => (value.title));
+    return (getTitles);
 };
 
 // Do not edit below this line
